7e5ec47efcb2ce07137036a8fcc92f5c3dde207a
김선중 <@DESKTOP-3A2428O>
2020-06-25 15:15:53 +0900

    선중

--- src/com/escapooh/QuizPage/model/dao/QuizFunction.java
+++ src/com/escapooh/QuizPage/model/dao/QuizFunction.java
@@ -1,13 +1,32 @@
 package com.escapooh.QuizPage.model.dao;
 
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import javax.swing.ImageIcon;
+
 public class QuizFunction {
 
+	public void  Reducedlife() {
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 	
+	 Thread th;
+	    
+	    Toolkit tk = Toolkit.getDefaultToolkit();
 	
-	
-	
+	    Image  fullLife_img1 = new ImageIcon("images/하트_ full 1.png").getImage() ;
+	    Image   fullLife_img2 = new ImageIcon("images/하트_ full 1.png").getImage();
+	    
+	    
 }
--- src/com/escapooh/QuizPage/view/Run.java
+++ src/com/escapooh/QuizPage/view/Run.java
@@ -10,38 +10,9 @@ public class Run {
 	//임시용 메인메소드
 	public static void main(String[] args) {
 		
-		//퀴즈 왼쪽 화면 1
-		//QuizLeftScreen1 back = new QuizLeftScreen1();
-		//back.main1();
 		
-		//퀴즈 왼쪽 화면2
-		//QuizLeftScreen2 back2 = new QuizLeftScreen2();
-		//back2.main2();
 		
-		//퀴즈 오른쪽 화면 1
-		//QuizRightScreen1 back3 = new QuizRightScreen1();
-		//back3.main3();
 		
-		//퀴즈 오른쪽 하면 2
-		//QuizRightScreen2 back4 = new QuizRightScreen2();
-		//back4.main4();
 		
-		//vo 쪽 퀴즈 입출력 실행용
-		//QuizLeft1 qi = new QuizLeft1();
-		//qi.QuizTextLeft1();
-		//qi.QuizTextLeft1out();
-		
-		//QuizLeft2 qi2 = new QuizLeft2();
-		//qi2.QuizTextLeft2();
-		//qi2.QuizTextLeft2out();
-		
-		//QuizRight1 qi3 = new QuizRight1();
-		//qi3.QuizTextRight1();
-		//qi3.QuizTextRight1out();
-		
-		//QuizRight2 qi4 = new QuizRight2();
-		//qi4.QuizTextRight2();
-		//qi4.QuizTextRight2out();
 	}
-
 }
